ed654aefe54af0b70f9fa00359809c496492f328
keene5 <31411970+keene5@users.noreply.github.com>
2024-11-05 09:41:54 -0500

    4 commit

--- routes/external_routes.js
+++ routes/external_routes.js
@@ -9,7 +9,8 @@ const apiKey = '20dc12da8ef34cfb8252bd9b0a8401b4';
 externalRouter.get("/getNews/:query", async (req, res) => {
   try {
     const query = req.params.query;
-    getNewsFromSource(query)
+    const endpoint = `${urlToNews}?sources=${query}&apiKey=${apiKey}`;
+    getNewsFromSource(endpoint)
       .then(data => {
         console.log(data);
         if (data && data.articles) {
@@ -32,8 +33,38 @@ externalRouter.get("/getNews/:query", async (req, res) => {
   }
 });
 
-function getNewsFromSource(topic) {
-  const endpoint = `${urlToNews}?sources=${topic}&apiKey=${apiKey}`;
+
+externalRouter.post("/postNews", async (req, res) => {
+
+
+  try {
+    const query = req.body;
+    let queryObjectWithApiKey = addApiKey(query);
+    let url = createUrlFromQueryObject(queryObjectWithApiKey);
+    getNewsFromSource(url)
+      .then(data => {
+        console.log(data);
+        if (data && data.articles) {
+          const articles = data.articles;
+          const extractedData = articles.map(article => ({
+            title: article.title,
+            url: article.url
+          }));
+          return res.status(200).json(extractedData);
+        }
+        return res.status(404).json({ message: "No News Found" });
+      })
+      .catch(error => {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message });
+      });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+function getNewsFromSource(endpoint) {
 
   const axiosConfig = {
     httpsAgent: new https.Agent({ rejectUnauthorized: false })
@@ -46,6 +77,16 @@ function getNewsFromSource(topic) {
     .catch(error => {
       throw new Error(error.message);
     });
+}
+
+
+function addApiKey(queryObject){
+  return {...queryObject, apiKey: apiKey}
+}
+function createUrlFromQueryObject(queryObjectWithApiKey) {
+  const queryString = new URLSearchParams(queryObjectWithApiKey).toString();
+  const url = urlToNews + "?" + queryString;
+  return url;
 }
 
 
